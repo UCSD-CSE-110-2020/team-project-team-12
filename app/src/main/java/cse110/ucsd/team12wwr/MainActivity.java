5cee66e4c6a76757f48655f2974e1d576753235e
kellykelp <kelo@s.sfusd.edu>
2020-02-06 15:13:50 -0800

    Conversion Successful

--- app/src/main/java/cse110/ucsd/team12wwr/MainActivity.java
+++ app/src/main/java/cse110/ucsd/team12wwr/MainActivity.java
@@ -1,5 +1,6 @@
 package cse110.ucsd.team12wwr;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -11,8 +12,18 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
+
+    SharedPreferences spf;
+    int totalHeight;
+    final int HEIGHT_FACTOR = 12;
+    final double STRIDE_CONVERSION = 0.413;
+    final int MILE_FACTOR = 63360;
+    double strideLength;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,14 +32,33 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        spf = getSharedPreferences("height", MODE_PRIVATE);
+        int feet = spf.getInt("feet", 0);
+        int inches = spf.getInt("inches", 0);
+
+        System.out.println("feet: " + feet + " inches: "  + inches);
+
+        totalHeight = inches + ( HEIGHT_FACTOR * feet );
+
+        System.out.println("YOU'RE " + totalHeight + " INCHES TALL WOWOWOWOWOWOWWOWOWO");
+
+        strideLength = totalHeight * STRIDE_CONVERSION;
+
+        DecimalFormat df = new DecimalFormat("#.##");
+        System.out.println("YOUR AVERAGE STRIDE LENGTH IS " + df.format(strideLength) + "QIWIWIWIWIQWIWIWHJRUAEISBFIUAEB");
+
+        System.out.println(df.format(MILE_FACTOR/strideLength));
+
+//        FloatingActionButton fab = findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+
+        //Toast.makeText(MainActivity.this, )
     }
 
     @Override
--- app/src/main/java/cse110/ucsd/team12wwr/StartPage.java
+++ app/src/main/java/cse110/ucsd/team12wwr/StartPage.java
@@ -1,5 +1,7 @@
 package cse110.ucsd.team12wwr;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -10,6 +12,7 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
 public class StartPage extends AppCompatActivity {
@@ -22,17 +25,40 @@ public class StartPage extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         // Set up the dropdown menus for height
-        Spinner foot_dropdown = findViewById(R.id.feet_spinner);
-        Integer[] foot_items = new Integer[]{0, 1, 2, 3, 4, 5, 6, 7, 8};
+        final Spinner foot_dropdown = findViewById(R.id.feet_spinner);
+        final Integer[] foot_items = new Integer[]{0, 1, 2, 3, 4, 5, 6, 7, 8};
         ArrayAdapter<Integer> feet_adapter = new ArrayAdapter<Integer>(this, android.R.layout.simple_spinner_item, foot_items);
         foot_dropdown.setAdapter(feet_adapter);
 
-        Spinner inch_dropdown = findViewById(R.id.inch_spinner);
-        Integer[] inch_items = new Integer[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+        final Spinner inch_dropdown = findViewById(R.id.inch_spinner);
+        final Integer[] inch_items = new Integer[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
         ArrayAdapter<Integer> inch_adapter = new ArrayAdapter<Integer>(this, android.R.layout.simple_spinner_item, inch_items);
         inch_dropdown.setAdapter(inch_adapter);
 
-        //Button launchMainPage =
+        // Button to launch new page
+        Button launchMainPage = (Button) findViewById(R.id.enter_button);
+
+        launchMainPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                int feetIndex = foot_dropdown.getSelectedItemPosition();
+                int feetValue = Integer.valueOf(foot_items[feetIndex]);
+
+                int inchIndex = inch_dropdown.getSelectedItemPosition();
+                int inchValue = Integer.valueOf(inch_items[inchIndex]);
+
+                SharedPreferences sharedPreferences = getSharedPreferences("height", MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+
+                editor.putInt("feet", feetValue);
+                editor.putInt("inches", inchValue);
+
+                editor.apply();
+
+                launchMainActivity(sharedPreferences);
+            }
+        });
 //        FloatingActionButton fab = findViewById(R.id.fab);
 //        fab.setOnClickListener(new View.OnClickListener() {
 //            @Override
@@ -41,6 +67,14 @@ public class StartPage extends AppCompatActivity {
 //                        .setAction("Action", null).show();
 //            }
 //        });
+    }
+
+    public void launchMainActivity(SharedPreferences spf) {
+
+        Intent intent = new Intent(this, MainActivity.class);
+        //intent.putExtra("feet", spf.getString("feet", "0"));
+        //intent.putExtra("inches", spf.getString("inches", "0"));
+        startActivity(intent);
     }
 
 }
