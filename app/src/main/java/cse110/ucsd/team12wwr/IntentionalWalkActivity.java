06a46d27e5abf4c47a33753d778827d2abfa16cf
Nicholas Alexander Limit <nicholasalimit@gmail.com>
2020-02-06 15:44:58 -0800

    Added experimental stopwatch

--- app/src/main/java/cse110/ucsd/team12wwr/IntentionalWalkActivity.java
+++ app/src/main/java/cse110/ucsd/team12wwr/IntentionalWalkActivity.java
@@ -8,26 +8,63 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.os.Handler;
+import android.os.SystemClock;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class IntentionalWalkActivity extends AppCompatActivity {
+    Handler handler;
+    long timeBuff, msTime, startTime;
+    TextView stopwatchText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intentional_walk);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        Button debugStartButton = findViewById(R.id.debug_btn_start_walk);
+        Button stopButton = findViewById(R.id.btn_stop_walk);
+        stopwatchText = findViewById(R.id.text_time_value);
+
+        handler = new Handler();
+
+        debugStartButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                startTime = SystemClock.uptimeMillis();
+
+                handler.postDelayed(runnable, 0);
             }
         });
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        stopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                timeBuff += msTime;
+                handler.removeCallbacks(runnable);
+            }
+        });
+
     }
+
+    public Runnable runnable = new Runnable() {
+        public void run() {
+            msTime = SystemClock.uptimeMillis() - startTime;
+
+            long updateTime = timeBuff + msTime;
+
+            int seconds = (int) (updateTime / 1000);
+            int minutes = seconds / 60;
+            seconds %= 60;
+
+            int milliseconds = (int) (updateTime % 1000);
+
+            stopwatchText.setText(String.format("%02d:%02d:%03d", minutes, seconds, milliseconds));
+            handler.postDelayed(this, 0);
+        }
+    };
+
 
 }
