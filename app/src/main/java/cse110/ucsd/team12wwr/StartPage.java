d487950bf1edcf1cfcc1836da606144958193e7f
kellykelp <kelo@s.sfusd.edu>
2020-02-06 14:33:27 -0800

    Added fields on start page

--- app/src/main/java/cse110/ucsd/team12wwr/StartPage.java
+++ app/src/main/java/cse110/ucsd/team12wwr/StartPage.java
@@ -9,6 +9,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class StartPage extends AppCompatActivity {
 
@@ -19,14 +21,26 @@ public class StartPage extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        // Set up the dropdown menus for height
+        Spinner foot_dropdown = findViewById(R.id.feet_spinner);
+        Integer[] foot_items = new Integer[]{0, 1, 2, 3, 4, 5, 6, 7, 8};
+        ArrayAdapter<Integer> feet_adapter = new ArrayAdapter<Integer>(this, android.R.layout.simple_spinner_item, foot_items);
+        foot_dropdown.setAdapter(feet_adapter);
+
+        Spinner inch_dropdown = findViewById(R.id.inch_spinner);
+        Integer[] inch_items = new Integer[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+        ArrayAdapter<Integer> inch_adapter = new ArrayAdapter<Integer>(this, android.R.layout.simple_spinner_item, inch_items);
+        inch_dropdown.setAdapter(inch_adapter);
+
+        //Button launchMainPage =
+//        FloatingActionButton fab = findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
     }
 
 }
