ed50e01b08fd0033597d58441dc158bd10f1877b
Nicholas Alexander Limit <nicholasalimit@gmail.com>
2020-02-06 19:08:38 -0800

    Renamed variables and added cancel check to start button

--- app/src/main/java/cse110/ucsd/team12wwr/IntentionalWalkActivity.java
+++ app/src/main/java/cse110/ucsd/team12wwr/IntentionalWalkActivity.java
@@ -3,13 +3,8 @@ package cse110.ucsd.team12wwr;
 import android.os.AsyncTask;
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
-import android.os.Handler;
 import android.os.SystemClock;
 import android.view.View;
 import android.widget.Button;
@@ -18,7 +13,8 @@ import android.widget.TextView;
 public class IntentionalWalkActivity extends AppCompatActivity {
     TextView stopwatchText;
     private AsyncTaskRunner runner;
-    long timeBuff, msTime;
+    long timeWhenPaused, timeElapsed;
+    // TODO inject dependency on CLOCK
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +28,10 @@ public class IntentionalWalkActivity extends AppCompatActivity {
         debugStartButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
+                if (runner != null) {
+                    runner.cancel(true);
+                }
+                
                 runner = new AsyncTaskRunner();
                 runner.execute();
             }
@@ -40,7 +40,7 @@ public class IntentionalWalkActivity extends AppCompatActivity {
         stopButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                timeBuff += msTime;
+                timeWhenPaused += timeElapsed;
                 if (runner != null) {
                     runner.cancel(true);
                 }
@@ -59,39 +59,32 @@ public class IntentionalWalkActivity extends AppCompatActivity {
         @Override
         protected String doInBackground(String... params) {
             while (true){
-                msTime = SystemClock.uptimeMillis() - startTime;
-
-                long updateTime = timeBuff + msTime;
+                timeElapsed = SystemClock.uptimeMillis() - startTime;
+                long updateTime = timeWhenPaused + timeElapsed;
 
                 int seconds = (int) (updateTime / 1000);
                 int minutes = (seconds / 60)  % 60;
                 int hours = (seconds / 3600)  % 60;
                 seconds %= 60;
 
-                publishProgress(String.format("Async %02d:%02d:%02d", hours, minutes, seconds));
+                publishProgress(String.format("%02d:%02d:%02d", hours, minutes, seconds));
                 try {
                     Thread.sleep(1000);
                 } catch (Exception e) {
                     return e.toString();
                 }
+
+                if (isCancelled()) {
+                    break;
+                }
             }
-
+            return "";
         }
-
-//        @Override
-//        protected void onPostExecute(String result) {
-//            // execution of result of Long time consuming operation
-//            stopwatchText.setText(result);
-//        }
 
         @Override
         protected void onProgressUpdate(String... text) {
             stopwatchText.setText(text[0]);
         }
 
-//        @Override
-//        protected void onCancelled() {
-//            stopwatchText.setText("Cancelled");
-//        }
     }
 }
