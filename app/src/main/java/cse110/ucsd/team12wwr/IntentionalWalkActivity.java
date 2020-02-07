c4b633ec035b63607fc5b0048be6fb8fcbf310f1
Nicholas Alexander Limit <nicholasalimit@gmail.com>
2020-02-06 16:13:40 -0800

    Migrated to AsyncTask

--- app/src/main/java/cse110/ucsd/team12wwr/IntentionalWalkActivity.java
+++ app/src/main/java/cse110/ucsd/team12wwr/IntentionalWalkActivity.java
@@ -1,5 +1,6 @@
 package cse110.ucsd.team12wwr;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -15,9 +16,9 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class IntentionalWalkActivity extends AppCompatActivity {
-    Handler handler;
-    long timeBuff, msTime, startTime;
     TextView stopwatchText;
+    private AsyncTaskRunner runner;
+    long timeBuff, msTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,14 +29,11 @@ public class IntentionalWalkActivity extends AppCompatActivity {
         Button stopButton = findViewById(R.id.btn_stop_walk);
         stopwatchText = findViewById(R.id.text_time_value);
 
-        handler = new Handler();
-
         debugStartButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                startTime = SystemClock.uptimeMillis();
-
-                handler.postDelayed(runnable, 0);
+                runner = new AsyncTaskRunner();
+                runner.execute();
             }
         });
 
@@ -43,27 +41,57 @@ public class IntentionalWalkActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 timeBuff += msTime;
-                handler.removeCallbacks(runnable);
+                if (runner != null) {
+                    runner.cancel(true);
+                }
             }
         });
     }
 
-    public Runnable runnable = new Runnable() {
-        public void run() {
-            msTime = SystemClock.uptimeMillis() - startTime;
+    private class AsyncTaskRunner extends AsyncTask<String, String, String> {
+        private long startTime;
 
-            long updateTime = timeBuff + msTime;
-
-            int seconds = (int) (updateTime / 1000);
-            int minutes = seconds / 60;
-            seconds %= 60;
-
-            int milliseconds = (int) (updateTime % 1000);
-
-            stopwatchText.setText(String.format("%02d:%02d:%03d", minutes, seconds, milliseconds));
-            handler.postDelayed(this, 0);
+        @Override
+        protected void onPreExecute() {
+            startTime = SystemClock.uptimeMillis();
         }
-    };
 
+        @Override
+        protected String doInBackground(String... params) {
+            while (true){
+                msTime = SystemClock.uptimeMillis() - startTime;
 
+                long updateTime = timeBuff + msTime;
+
+                int seconds = (int) (updateTime / 1000);
+                int minutes = (seconds / 60)  % 60;
+                int hours = (seconds / 3600)  % 60;
+                seconds %= 60;
+
+                publishProgress(String.format("Async %02d:%02d:%02d", hours, minutes, seconds));
+                try {
+                    Thread.sleep(1000);
+                } catch (Exception e) {
+                    return e.toString();
+                }
+            }
+
+        }
+
+//        @Override
+//        protected void onPostExecute(String result) {
+//            // execution of result of Long time consuming operation
+//            stopwatchText.setText(result);
+//        }
+
+        @Override
+        protected void onProgressUpdate(String... text) {
+            stopwatchText.setText(text[0]);
+        }
+
+//        @Override
+//        protected void onCancelled() {
+//            stopwatchText.setText("Cancelled");
+//        }
+    }
 }
