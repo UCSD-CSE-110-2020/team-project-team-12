cd051502e1817faf8f9718e7abecd6aa53ccb3bb
Nicholas Alexander Limit <nicholasalimit@gmail.com>
2020-02-07 19:13:46 -0800

    Integrated distance formula, added dummy step incrementer

--- app/src/main/java/cse110/ucsd/team12wwr/IntentionalWalkActivity.java
+++ app/src/main/java/cse110/ucsd/team12wwr/IntentionalWalkActivity.java
@@ -1,5 +1,6 @@
 package cse110.ucsd.team12wwr;
 
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
 
@@ -11,12 +12,19 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class IntentionalWalkActivity extends AppCompatActivity {
-    private TextView stopwatchText;
+    // TODO code repetition
+    private final int HEIGHT_FACTOR = 12;
+    private final double STRIDE_CONVERSION = 0.413;
+    private final int MILE_FACTOR = 63360;
+
+    private TextView stopwatchText, stepsText, distanceText;
     private AsyncTaskRunner runner;
     private int timeWhenPaused, timeElapsed;
+    private double strideLength;
+
+    private int temporaryNumSteps;
 
     private iClock clock;
-    // TODO inject dependency on CLOCK
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,10 +32,21 @@ public class IntentionalWalkActivity extends AppCompatActivity {
         setContentView(R.layout.activity_intentional_walk);
         clock = new DeviceClock();
 
+        // TODO this is code repetition, should just declare getStrideLength() somewhere
+        SharedPreferences spf = getSharedPreferences("height", MODE_PRIVATE);
+        int feet = spf.getInt("feet", 0);
+        int inches = spf.getInt("inches", 0);
+        int totalHeight = inches + ( HEIGHT_FACTOR * feet );
+        strideLength = totalHeight * STRIDE_CONVERSION;
+
         final Button pauseButton = findViewById(R.id.btn_pause_walk);
         final Button continueButton = findViewById(R.id.btn_continue_walk);
         final Button stopButton = findViewById(R.id.btn_stop_walk);
+
         stopwatchText = findViewById(R.id.text_time_value);
+        stepsText = findViewById(R.id.text_steps_value);
+        distanceText = findViewById(R.id.text_distance_value);
+
 
         pauseButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -64,6 +83,11 @@ public class IntentionalWalkActivity extends AppCompatActivity {
         this.clock = clock;
     }
 
+    protected int getNumSteps() {
+        temporaryNumSteps += 10;
+        return temporaryNumSteps;
+    }
+
     private class AsyncTaskRunner extends AsyncTask<String, String, String> {
         private int startTime;
 
@@ -82,7 +106,17 @@ public class IntentionalWalkActivity extends AppCompatActivity {
                 int minutes = (updateTime / 60)  % 60;
                 int seconds = updateTime % 60;
 
-                publishProgress(String.format("%02d:%02d:%02d", hours, minutes, seconds));
+                String timeString = String.format("%02d:%02d:%02d", hours, minutes, seconds);
+
+                //TODO replace with Thuy's functionality
+                int numSteps = getNumSteps();
+                String stepsString = String.format("%d", numSteps);
+
+                double distance = (strideLength / MILE_FACTOR) * numSteps;
+                String distanceString = String.format("%.2f mi", distance);
+
+                publishProgress(timeString, stepsString, distanceString);
+
                 try {
                     Thread.sleep(1000);
                 } catch (Exception e) {
@@ -99,6 +133,8 @@ public class IntentionalWalkActivity extends AppCompatActivity {
         @Override
         protected void onProgressUpdate(String... text) {
             stopwatchText.setText(text[0]);
+            stepsText.setText(text[1]);
+            distanceText.setText(text[2]);
         }
 
     }
