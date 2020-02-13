5cc11e838f95b2e684521e269f078fef97fb5be9
Nicholas Alexander Limit <nicholasalimit@gmail.com>
2020-02-13 09:56:15 -0800

    Added Route schema

---
+++ app/src/main/java/cse110/ucsd/team12wwr/RouteDao.java
@@ -0,0 +1,20 @@
+package cse110.ucsd.team12wwr;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
+public interface RouteDao {
+    @Insert
+    void insertAll(Route... r);
+
+    @Delete
+    void delete(Route r);
+
+    @Query("SELECT * FROM Route r")
+    List<Route> retrieveAllRoutes();
+}
--- app/src/main/java/cse110/ucsd/team12wwr/WalkDatabase.java
+++ app/src/main/java/cse110/ucsd/team12wwr/WalkDatabase.java
@@ -6,10 +6,7 @@ import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
-@Database(entities = {Walk.class}, version = 1, exportSchema = false)
+@Database(entities = {Walk.class, Route.class}, version = 1, exportSchema = false)
 public abstract class WalkDatabase extends RoomDatabase {
     private static volatile WalkDatabase INSTANCE;
     public abstract WalkDao walkDao();
