99c0e683cc1ab00d58341216d7811b151fabf62a
jcbibat <33525534+jcbibat@users.noreply.github.com>
2020-03-04 00:50:28 -0800

    Integrated Split Routes Screen with Button

--- app/src/main/java/cse110/ucsd/team12wwr/TeamIndividRoutes.java
+++ app/src/main/java/cse110/ucsd/team12wwr/TeamIndividRoutes.java
@@ -1,9 +1,14 @@
 package cse110.ucsd.team12wwr;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.tabs.TabLayout;
 
+import androidx.annotation.NonNull;
 import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -16,7 +21,34 @@ public class TeamIndividRoutes extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_team_individ_routes);
 
+        BottomNavigationView navigation = findViewById(R.id.nav_view);
+        Menu menu = navigation.getMenu();
+        MenuItem menuItem = menu.getItem(1);
+        menuItem.setChecked(true);
 
+        navigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.navigation_home:
+                        finish();
+                        break;
+                    case R.id.navigation_routes:
+                        break;
+                    case R.id.navigation_walk:
+                        finish();
+                        launchActivity();
+
+                        break;
+                    case R.id.navigation_teams:
+                        finish();
+                        launchTeamScreenActivity();
+
+                        break;
+                }
+                return false;
+            }
+        });
 
         SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
         ViewPager viewPager = findViewById(R.id.view_pager);
@@ -24,5 +56,15 @@ public class TeamIndividRoutes extends AppCompatActivity {
         TabLayout tabs = findViewById(R.id.tabs);
         tabs.setupWithViewPager(viewPager);
 
+    }
+
+    public void launchActivity() {
+        Intent intent = new Intent(this, IntentionalWalkActivity.class);
+        startActivity(intent);
+    }
+
+    public void launchTeamScreenActivity() {
+        Intent intent = new Intent(this, TeamScreen.class);
+        startActivity(intent);
     }
 }
