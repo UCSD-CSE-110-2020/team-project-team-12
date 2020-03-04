d35daec7b39f4abbc5dde2c1bb82cec503973acb
Nicholas Alexander Limit <nicholasalimit@gmail.com>
2020-03-03 23:14:07 -0800

    US Firestore: Task Refactor Unit Tests – Added mock behavior when unit testing

--- app/src/main/java/cse110/ucsd/team12wwr/firebase/FirebaseWalkDao.java
+++ app/src/main/java/cse110/ucsd/team12wwr/firebase/FirebaseWalkDao.java
@@ -5,10 +5,21 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import cse110.ucsd.team12wwr.MainActivity;
+
 public class FirebaseWalkDao {
-    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    FirebaseFirestore db;
+    public FirebaseWalkDao() {
+        if (!MainActivity.unitTestFlag) {
+            db = FirebaseFirestore.getInstance();
+        }
+    }
 
     public void insertAll(Walk... w) {
+        if (db == null) {
+            return;
+        }
+
         for (Walk walk : w) {
             db.collection("walks").add(walk);
         }
@@ -16,11 +27,19 @@ public class FirebaseWalkDao {
 
 //    @Query("SELECT * FROM Walk w WHERE NOT EXISTS (SELECT a.time FROM Walk a WHERE a.time > w.time)")
     public Task<QuerySnapshot> findNewestEntries() {
+        if (db == null) {
+            return new MockTask<>();
+        }
+
         return db.collection("walks").orderBy("time", Query.Direction.DESCENDING).get();
     }
 
 //    @Query("SELECT * FROM Walk w WHERE w.routeName=:routeName ORDER BY time DESC")
     public Task<QuerySnapshot> findByRouteName(String routeName) {
+        if (db == null) {
+            return new MockTask<>();
+        }
+
         return db.collection("walks").whereEqualTo("routeName", routeName)
                 .orderBy("time", Query.Direction.DESCENDING).get();
     }
