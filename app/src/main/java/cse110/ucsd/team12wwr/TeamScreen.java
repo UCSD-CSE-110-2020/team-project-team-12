714e5ae223bd41465749df38809a482a2710bc36
jcbibat <33525534+jcbibat@users.noreply.github.com>
2020-03-03 22:46:10 -0800

    US #1 Button Stash

--- app/src/main/java/cse110/ucsd/team12wwr/TeamScreen.java
+++ app/src/main/java/cse110/ucsd/team12wwr/TeamScreen.java
@@ -10,10 +10,6 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 
 public class TeamScreen extends AppCompatActivity {
 
