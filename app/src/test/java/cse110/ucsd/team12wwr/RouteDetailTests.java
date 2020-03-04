715e8b50c636f3dae62017e12bdaeff1c4951eca
Nicholas Alexander Limit <nicholasalimit@gmail.com>
2020-03-03 21:16:03 -0800

    US Firebase: Task Refactor Legacy Code – Removed unused import statements and Room DB imports

--- app/src/test/java/cse110/ucsd/team12wwr/RouteDetailTests.java
+++ app/src/test/java/cse110/ucsd/team12wwr/RouteDetailTests.java
@@ -12,30 +12,12 @@ import org.apache.tools.ant.Main;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.robolectric.Robolectric;
-import org.robolectric.shadows.ShadowActivity;
-import org.robolectric.shadows.ShadowIntent;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
-import androidx.test.core.app.ActivityScenario;
 import androidx.test.core.app.ApplicationProvider;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
-import cse110.ucsd.team12wwr.database.Route;
-import cse110.ucsd.team12wwr.database.RouteDao;
-import cse110.ucsd.team12wwr.database.WWRDatabase;
-import cse110.ucsd.team12wwr.database.Walk;
-import cse110.ucsd.team12wwr.database.WalkDao;
 
-import static junit.framework.TestCase.assertNotNull;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotEquals;
-import static org.junit.Assert.assertThat;
-import static org.junit.Assert.assertTrue;
-import static org.robolectric.Shadows.shadowOf;
 
 @RunWith(AndroidJUnit4.class)
 public class RouteDetailTests {
