e49d21173e2cc242e9372dc9c64d783e86c73a23
Nicholas Alexander Limit <nicholasalimit@gmail.com>
2020-03-13 18:38:25 -0700

    US 6: Updated user vote map upon vote button click

--- app/src/main/java/cse110/ucsd/team12wwr/recycler/RecycleAdapter.java
+++ app/src/main/java/cse110/ucsd/team12wwr/recycler/RecycleAdapter.java
@@ -4,6 +4,7 @@ import cse110.ucsd.team12wwr.MainActivity;
 import cse110.ucsd.team12wwr.ProposedWalkScreen;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,8 +16,12 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 
+import java.util.Map;
+
 import cse110.ucsd.team12wwr.R;
 import cse110.ucsd.team12wwr.firebase.DaoFactory;
+import cse110.ucsd.team12wwr.firebase.Schedule;
+import cse110.ucsd.team12wwr.firebase.ScheduleDao;
 import cse110.ucsd.team12wwr.firebase.User;
 import cse110.ucsd.team12wwr.firebase.UserDao;
 
@@ -96,6 +101,7 @@ public class RecycleAdapter extends RecyclerView.Adapter<RecycleAdapter.MyViewHo
             TextView name = containerView.findViewById(R.id.vote_name);
             String displayedName = name.getText().toString();
 
+            Schedule.Vote vote = Schedule.Vote.ABSTAINED;
             if (displayedName.equals(currUserFullName)) {
                 switch(buttonId) {
                     case R.id.yes:
@@ -103,6 +109,7 @@ public class RecycleAdapter extends RecyclerView.Adapter<RecycleAdapter.MyViewHo
                             yes.setButtonDrawable(R.drawable.button_yes_on);
                             dontWant.setButtonDrawable(R.drawable.button_no_off);
                             badTime.setButtonDrawable(R.drawable.button_no_off);
+                            vote = Schedule.Vote.ACCEPTED;
                         }
                         break;
                     case R.id.bad_time:
@@ -110,6 +117,7 @@ public class RecycleAdapter extends RecyclerView.Adapter<RecycleAdapter.MyViewHo
                             yes.setButtonDrawable(R.drawable.button_yes_off);
                             dontWant.setButtonDrawable(R.drawable.button_no_off);
                             badTime.setButtonDrawable(R.drawable.button_no_on);
+                            vote = Schedule.Vote.DECLINED_CONFLICT;
                         }
                         break;
                     case R.id.dont_want:
@@ -117,9 +125,37 @@ public class RecycleAdapter extends RecyclerView.Adapter<RecycleAdapter.MyViewHo
                             yes.setButtonDrawable(R.drawable.button_yes_off);
                             dontWant.setButtonDrawable(R.drawable.button_no_on);
                             badTime.setButtonDrawable(R.drawable.button_no_off);
+                            vote = Schedule.Vote.DECLINED_DIFFICULTY;
                         }
                         break;
                 }
+                final Schedule.Vote finalVote = vote;
+
+                DaoFactory.getUserDao().findUserByID(MainActivity.userEmail, task-> {
+                    if (task.isSuccessful())  {
+                        User u = null;
+                        for (QueryDocumentSnapshot doc : task.getResult()) {
+                            u = doc.toObject(User.class);
+                        }
+                        if (u != null) {
+                            String teamId = u.teamID;
+                            ScheduleDao scheduleDao = DaoFactory.getScheduleDao();
+                            scheduleDao.findScheduleByTeam(teamId, task2 -> {
+                               if (task2.isSuccessful()) {
+                                   Schedule s = null;
+                                   for (QueryDocumentSnapshot doc : task2.getResult()) {
+                                       s = doc.toObject(Schedule.class);
+                                   }
+                                   if (s != null) {
+                                       Map<String, Schedule.Vote> userVoteMap = s.userVoteMap;
+                                       userVoteMap.put(MainActivity.userEmail, finalVote);
+                                       scheduleDao.updateVoteMap(teamId, userVoteMap);
+                                   }
+                               }
+                            });
+                        }
+                    }
+                });
             }
         }
     }
