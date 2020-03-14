8c55014a1a308df74203a73599ed9db438ffd380
Nicholas Alexander Limit <nicholasalimit@gmail.com>
2020-03-13 18:06:21 -0700

    US 6: Redirected scheduled walk launch sequence

--- app/src/main/java/cse110/ucsd/team12wwr/ProposedWalkScreen.java
+++ app/src/main/java/cse110/ucsd/team12wwr/ProposedWalkScreen.java
@@ -49,35 +49,21 @@ public class ProposedWalkScreen extends AppCompatActivity {
                         for (QueryDocumentSnapshot document : task2.getResult()) {
                             userList.add(document.toObject(User.class));
                         }
+                        itemList = new ArrayList<>();
+
+                        for (User u : userList) {
+                            Item item = new Item();
+                            item.setName(u.firstName + " " + u.lastName);
+                            itemList.add(item);
+                        }
+
+                        recycleAdapter = new RecycleAdapter(this);
+
+                        recyclerView.setAdapter(recycleAdapter);
+                        recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext(), LinearLayoutManager.VERTICAL, false));
                     });
                 }
             }
         });
-
-        itemList = new ArrayList<>();
-
-        for (User u : userList) {
-            Item item = new Item();
-            item.setName(u.firstName + " " + u.lastName);
-            itemList.add(item);
-        }
-
-//        itemList = new ArrayList<>();
-//        Item item = new Item();
-//        item.setName("Jeffrey");
-//        itemList.add(item);
-//
-//        Item item1 = new Item();
-//        item1.setName("Geoffrey");
-//        itemList.add(item1);
-//
-//        Item item2 = new Item();
-//        item2.setName("Joffrey");
-//        itemList.add(item2);
-
-        recycleAdapter = new RecycleAdapter(this);
-
-        recyclerView.setAdapter(recycleAdapter);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext(), LinearLayoutManager.VERTICAL, false));
     }
 }
